e507dd3ee69d81b78bdcc838f529a74d83c6997a
Grzegorz Zakrzewski <zakrzew12@gmail.com>
2024-11-15 20:36:26 +0100

    ;

--- src/experiment/ParhydraExperiment.py
+++ src/experiment/ParhydraExperiment.py
@@ -1,6 +1,7 @@
 from typing import Type
 
 import numpy as np
+from ConfigSpace import Configuration
 
 from src.experiment import Experiment
 from src.instance import Instance, InstanceSet
@@ -25,29 +26,53 @@ class ParhydraExperiment(Experiment):
 
     def construct_portfolio(self, train_instances: InstanceSet) -> Portfolio:
         solvers = []
+        largest_marginal_contribution_solver = None
         for i in range(self.K):
             logger.info(f"Solver {i + 1}/{self.K}")
-            best_solver = None
+
             best_cost = np.inf
+            best_solver = None
+            attempt_solvers = []
             for _ in range(self.n):
                 logger.info(f"Attempt {_ + 1}/{self.n}")
 
-                new_solver = self.solver_class()
+                if largest_marginal_contribution_solver is not None:
+                    new_solver = largest_marginal_contribution_solver.copy()
+                else:
+                    new_solver = self.solver_class()
                 iteration_solvers = solvers + [new_solver]
                 portfolio = Portfolio.from_solver_list(iteration_solvers)
                 configuration_space = portfolio.get_configuration_space(i=i)
-
+                config = {f"{i}__{k}": v for k, v in new_solver.config.items()}
+                config = Configuration(configuration_space, values=config)
                 cost = self._configure_and_validate(
                     portfolio,
                     train_instances,
                     configuration_space,
+                    config,
                 )
-
+                attempt_solvers.append(portfolio[i])
                 logger.info(f"Attempt {_ + 1}/{self.n}: cost = {cost:.2f}")
                 if cost < best_cost:
-                    logger.info(f"New best solver found!")
                     best_cost = cost
-                    best_solver = new_solver
+                    best_solver = portfolio[i]
             solvers.append(best_solver)
+            logger.info(f"Solver {i + 1}/{self.K} - best cost = {best_cost:.2f}")
+
+            if i < self.K - 1:
+                largest_marginal_contribution_solver = None
+                best_cost = np.inf
+                for solver in attempt_solvers:
+                    if solver != best_solver:
+                        portfolio = Portfolio.from_solver_list(solvers + [solver])
+                        cost = portfolio.evaluate(
+                            train_instances,
+                            np.ones(shape=(self.K,)) * np.inf,
+                            "largest_marginal_contribution",
+                        )
+                        if cost < best_cost:
+                            best_cost = cost
+                            largest_marginal_contribution_solver = solver
+
         portfolio = Portfolio.from_solver_list(solvers)
         return portfolio
