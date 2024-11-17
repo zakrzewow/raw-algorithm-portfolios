f6f362f142e4f8b735ebc3e7a02193550676c3b0
Grzegorz Zakrzewski <zakrzew12@gmail.com>
2024-11-17 16:49:44 +0100

    ;

--- run_pcit.py
+++ run_pcit.py
@@ -8,14 +8,14 @@ from src.solver.TSP_LKH_Solver import TSP_LKH_Solver
 if __name__ == "__main__":
     train_instances, test_instances = TSP_InstanceSet.train_test_from_index_file(
         filepath=DATA_DIR / "TSP" / "CEPS_benchmark" / "index.json",
-        train_size=10,
+        train_size=30,
         seed=0,
     )
-    t_c = 400
-    t_v = 100
-    K = 2
-    n = 2
-    max_iter = 2
+    t_c = 1200
+    t_v = int(1 * 3600)
+    K = 4
+    n = 4
+    max_iter = 4
     solver_class = TSP_LKH_Solver
     instance_class = TSP_Instance
 
@@ -31,5 +31,5 @@ if __name__ == "__main__":
 
     best_portfolio = experiment.construct_portfolio(train_instances)
 
-    # remaining_time = np.ones(shape=(K,)) * np.inf
-    # best_portfolio.evaluate(test_instances, remaining_time, "test")
+    remaining_time = np.ones(shape=(K,)) * np.inf
+    best_portfolio.evaluate(test_instances, remaining_time, "test")
