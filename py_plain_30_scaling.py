000d9dc14cd7b2ec049258bc19a5560293622d4f
Grzegorz Zakrzewski <zakrzew12@gmail.com>
2025-03-03 19:40:33 +0100

    ;

--- py_plain_30_scaling.py
+++ py_plain_30_scaling.py
@@ -10,15 +10,15 @@ if __name__ == "__main__":
 
     test_instances = TSP_from_index_file(
         filepath=DATA_DIR / "TSP" / "TEST_600" / "index.json",
-        max_cost=26.6,
-        max_time=2.66,
+        max_cost=53.1,
+        max_time=5.31,
     )
     number_of_instances = N // 5
 
     instances = TSP_from_index_file(
         filepath=DATA_DIR / "TSP" / "TRAIN_200" / "index.json",
-        max_cost=2.3,
-        max_time=0.23,
+        max_cost=4.7,
+        max_time=0.47,
     )
     train_instances_200 = InstanceList()
     for i in range(5):
@@ -26,8 +26,8 @@ if __name__ == "__main__":
 
     instances = TSP_from_index_file(
         filepath=DATA_DIR / "TSP" / "TRAIN_400" / "index.json",
-        max_cost=10.9,
-        max_time=1.09,
+        max_cost=21.8,
+        max_time=2.18,
     )
     train_instances_400 = InstanceList()
     for i in range(5):
@@ -35,8 +35,8 @@ if __name__ == "__main__":
 
     instances = TSP_from_index_file(
         filepath=DATA_DIR / "TSP" / "TRAIN_600" / "index.json",
-        max_cost=26.6,
-        max_time=2.66,
+        max_cost=53.1,
+        max_time=5.31,
     )
     train_instances_600 = InstanceList()
     for i in range(5):
@@ -48,17 +48,31 @@ if __name__ == "__main__":
         portfolio=portfolio,
         instance_list=train_instances_200,
         prefix="config",
-        max_iter=75,
+        max_iter=25,
         calculate_features=False,
         estimator=None,
     )
-    for _ in aac.configure_iter():
-        if aac.iter == 25:
-            aac.update(instance_list=train_instances_400)
-        elif aac.iter == 50:
-            aac.update(instance_list=train_instances_600)
+    portfolio = aac.configure()
 
-    portfolio = aac._portfolio
+    aac = AAC(
+        portfolio=portfolio,
+        instance_list=train_instances_400,
+        prefix="config",
+        max_iter=25,
+        calculate_features=False,
+        estimator=None,
+    )
+    portfolio = aac.configure()
+
+    aac = AAC(
+        portfolio=portfolio,
+        instance_list=train_instances_600,
+        prefix="config",
+        max_iter=25,
+        calculate_features=False,
+        estimator=None,
+    )
+    portfolio = aac.configure()
 
     for i in range(100):
         portfolio.evaluate(
