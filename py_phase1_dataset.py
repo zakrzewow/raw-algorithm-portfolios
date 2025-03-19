61005fd1b60d5f55015f50778831dbb6479bc2fc
Grzegorz Zakrzewski <grzegorz.zakrzewski@respect.energy>
2025-03-19 15:33:59 +0100

    ;

--- py_phase1_dataset.py
+++ py_phase1_dataset.py
@@ -1,27 +1,32 @@
+import os
+
 from src.constant import DATA_DIR
-from src.instance.InstanceList import InstanceList
 from src.instance.TSP_Instance import TSP_from_index_file
 from src.solver.Portfolio import Portfolio
 from src.solver.TSP_LKH_Solver import TSP_LKH_Solver
 
 if __name__ == "__main__":
-    N = 100
+    generator = os.environ.get("GENERATOR").strip()
+    generator_to_i = {
+        "cluster_netgen": 0,
+        "comperssion": 200,
+        "expansion": 400,
+        "explosion": 600,
+        "grid": 800,
+    }
+    i = generator_to_i[generator]
 
-    all_instances = TSP_from_index_file(
+    instances = TSP_from_index_file(
         filepath=DATA_DIR / "TSP" / "TRAIN" / "index.json",
-        max_cost=3000.0,
-        max_time=300.0,
+        max_cost=10000.0,
+        max_time=1000.0,
     )
-
-    number_of_instances = N // 5
-    instances = InstanceList()
-    for i in range(5):
-        instances.extend(all_instances[i * 200 : i * 200 + number_of_instances])
+    instances = instances[i : i + 5]
 
     portfolio = Portfolio.from_solver_class(TSP_LKH_Solver, size=500)
     portfolio.evaluate(
         instance_list=instances,
         prefix="dataset",
-        calculate_features=True,
+        calculate_features=False,
         cache=True,
     )
