f74decb5d8901334de309887108221802a64bdb6
Grzegorz Zakrzewski <zakrzew12@gmail.com>
2025-05-27 22:28:02 +0200

    ;

--- src/database/queries.py
+++ src/database/queries.py
@@ -14,7 +14,7 @@ def get_model_training_data(db: DB) -> pd.DataFrame:
     join {db.SCHEMA.SOLVERS} on {db.SCHEMA.EVALUATIONS}.solver_id = {db.SCHEMA.SOLVERS}.id
     """
     df = db.query2df(query)
-    df = df.drop(columns=["id", "filepath", "optimum"])
+    df = df.drop(columns=["id", "filepath"])
     df = df.dropna()
     y = df["cost"].to_numpy()
     X = df.drop(columns="cost").to_numpy()
