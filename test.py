19fb10f11c570184e7f16ec77713f592026109bc
Grzegorz Zakrzewski <grzegorz.zakrzewski@respect.energy>
2025-05-27 11:25:05 +0200

    :

--- test.py
+++ test.py
@@ -2,12 +2,14 @@ from src.constant import DATA_DIR
 from src.instance.SAT_Instance import SAT_from_index_file
 from src.solver.SAT_Riss_Solver import SAT_Riss_Solver
 
-solver = SAT_Riss_Solver()
 instances = SAT_from_index_file(
     filepath=DATA_DIR / "SAT" / "index.json",
     max_cost=100.0,
     max_time=10.0,
 )
 
-for instance in instances:
-    solver.solve(instance, prefix="test", cache=False)
+for i in range(3):
+    solver = SAT_Riss_Solver()
+
+    for instance in instances:
+        solver.solve(instance, prefix=f"test-{i}", cache=False)
