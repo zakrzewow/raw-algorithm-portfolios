a7a70b89bdf4456ff9fadbc3f31b3c75cd91e0b1
Grzegorz Zakrzewski <grzegorz.zakrzewski@respect.energy>
2025-05-28 07:49:17 +0200

    ;

---
+++ py_sur_test.py
@@ -0,0 +1,43 @@
+from src.aac.AAC import AAC
+from src.aac.SurrogateEstimator import Estimator1
+from src.constant import DATA_DIR
+from src.database import DB
+from src.database.queries import get_model_training_data
+from src.instance.SAT_Instance import SAT_from_index_file
+from src.solver.Portfolio import Portfolio
+from src.solver.SAT_Riss_Solver import SAT_Riss_Solver
+
+if __name__ == "__main__":
+    ESTIMATOR_PCT = 0.5
+
+    instances = SAT_from_index_file(
+        filepath=DATA_DIR / "SAT" / "index.json",
+        max_cost=100.0,
+        max_time=10.0,
+    )
+
+    train_instances = instances[:5]
+
+    portfolio = Portfolio.from_solver_class(SAT_Riss_Solver, size=2)
+
+    aac = AAC(
+        portfolio=portfolio,
+        instance_list=train_instances,
+        prefix="config",
+        max_iter=15,
+        calculate_features=True,
+        estimator=None,
+    )
+
+    last_model_iter = 0
+
+    estimator = None
+    db = DB()
+    for _ in aac.configure_iter():
+        if aac.iter >= 5 and aac.iter - last_model_iter >= 5:
+            X, y = get_model_training_data(db)
+            estimator = Estimator1(max_cost=100.0, estimator_pct=ESTIMATOR_PCT)
+            estimator.fit(X, y)
+            estimator.log()
+            last_model_iter = aac.iter
+            aac.update(estimator=estimator)
